99813894809ef2b9dcb21de7e5dbe4f98169acc1
michałBazan <michalbaz@poczta.onet.pl>
2019-07-06 00:03:56 +0200

    prawie cały kontent diva 2, do kompletu brakuje slidera (strzałka do umiejscowienia)

--- index.php
+++ index.php
@@ -3,34 +3,43 @@
     <head>
         <meta charset="utf-8">
         <link href="css/styl.css" rel="stylesheet">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <!--css botstrap-->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <title>Home page</title>
     </head>
 
 <body>
-    <div class='container col-md-12' id="div1"></div>
+    <div class='container col-sm-12 ' id="div1"></div>
     
-    <div class='container col-md-12' id="div2">
-        <div class='container col-md-6 mx-auto'>
-            <div class='head mt-5' align='center'>Informacje o Nas</div>
-            <p align='center'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
-            laboris nisi ut aliquip ex ea commodo consequat.</p>
+        <div class='row'>
+            <div class='container col-sm-12' id="div2">
+                <div class='container col-md-5 mx-auto'>
+                <div class='head mt-5' align='center'>Informacje o Nas</div>
+                <p align='center'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+                laboris nisi ut aliquip ex ea commodo consequat.</p>
+                </div>
+        <div class='row mx-auto col-sm-8 mt-5 mb-5'>
+            <div class='col-sm-4 mt-2'><img class='obr img-fluid'  src='img/info1skalowane.png' alt='pic1'></div>
+            <div class='col-sm-4 mt-5 mb-3'><img class='obr img-fluid'  src='img/info2skalowane.png' alt='pic2'></div>
+            <div class='col-sm-4 mt-2'><img class='obr img-fluid'  src='img/info3skalowane.png' alt='pic3'></div>
+            </div>
+        <div class='row mx-auto col-sm-8 mt-3 mb-5'>
+                <div class='container-fluid col-sm-5 mt-5 ml-3' id='spacer'>
+                    <img class='obr img-fluid mx-auto my-auto' src='img/strzalka.png' alt='spacer'>
+                </div>
+            <div class='col-sm-6'>
+            <div class='head mt-5' align='left'>Spacer wirtualny</div>
+            <p align='left' >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p></div>
         </div>
-        <div class='container mt-5'>
-            <div class='col-md-4'>
-                <img src='img/info1skalowane.png' alt='obrazek 1'>
-            </div>
-            <div class='col-md-4 mt-5'>
-                <img src='img/info2skalowane.png' alt='obrazek 2'>
-            </div>
-            <div class='col-md-4'>
-                <img src='img/info3skalowane.png' alt='obrazek 3'>
-            </div>
-        </div>
 
+        <div class='col-sm-8 mx-auto'><div class='head mt-5 ml-2' align='left'>Zdjęcia z Instagram</div></div>
 
-
+    
+    
+    
+    </div>     
     </div>
     <div class='container col-md-12' id="div3"></div>
     <div class='container-fluid stopka'><p>Copyright 2019 - Michał Bazan & Eplee</p></div>
