3243554ff352b10615b4ec7cf2394c245e24e1bc
michałBazan <michalbaz@poczta.onet.pl>
2019-07-07 10:31:28 +0200

    wykonczenie trzeciej sekcji

--- js/skrypt.js
+++ js/skrypt.js
@@ -4,12 +4,3 @@ function powieksz(){
 function napisz(){
    
 }
-(function(){
-    var mapa = document.getElementById('mapa');
-    if(window.innerWidth<769){
-        mapa.height = 300;
-    }
-    else{
-        mapa.height = 520;
-    }
-}());
