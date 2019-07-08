f7248e171bab85bd1b8666349326b927a64601ef
michałBazan <michalbaz@poczta.onet.pl>
2019-07-08 10:02:27 +0200

    powiekszenie obrazka

--- js/skrypt.js
+++ js/skrypt.js
@@ -1,7 +1,10 @@
 //zdjęcia
 
 function powieksz(){
-    
+    var gal = document.getElementById('gal');
+    gal.style.display = 'block';
+    var img = document.getElementById('ramka').src ='img/zdjecia/spacer1merged.jpg';
+    var opis = document.getElementById('opis').textContent='Powiększenie spowodowane przyciskiem';
 }
 
 //formularz popup
@@ -9,10 +12,14 @@ function powieksz(){
 function napisz(){
    var msg = document.getElementById('msg');
    msg.style.display = 'block';
+   
 }
 function zamknij(){
     var msg = document.getElementById('msg');
-    msg.style.display = 'none';
+    var gal = document.getElementById('gal');
+    gal.style.display=msg.style.display = 'none';
+    var img = document.getElementById('ramka').src ='';
+    var opis = document.getElementById('opis').textContent='';
 }
 function walidacja(){
     var formularz = document.forms[0];
