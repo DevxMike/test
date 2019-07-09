b6a8eb92ef406d6227fd3c63e42b7dca56436a73
michałBazan <michalbaz@poczta.onet.pl>
2019-07-09 11:17:04 +0200

    poczatek animacji slidera

--- js/skrypt.js
+++ js/skrypt.js
@@ -31,3 +31,13 @@ function walidacja(){
         formularz.submit();
     }
 }
+var sciezki = ['../img/slides/ins1.jpg','../img/slides/ins2.jpg','../img/slides/ins3.jpg',
+'../img/slides/ins4.jpg','../img/slides/ins5.jpg','../img/slides/ins6.jpg'];
+
+var id = ['obr1','obr2','obr3','obr4','obr5','obr6'];
+var trans = 0;
+function move(){
+    trans+=16;
+    document.getElementById('slider').style= 'transition:0.6s; transform:translateX('+trans+'%);';
+}
+setInterval(move,2000);
