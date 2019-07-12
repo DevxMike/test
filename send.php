2a2514e6d5e06c22b1ba3bf6f58b428699a6f602
michałBazan <michalbaz@poczta.onet.pl>
2019-07-12 10:43:40 +0200

    wciaz nieznany problem z czcionka...

--- send.php
+++ send.php
@@ -2,9 +2,8 @@
     $od = $_POST['email']; 
     $tresc = $_POST['wiadomosc'];
     $_POST = array();
-    $msg = "Wiadomość od: ".$od."\n\n";
-    $msg .= $tresc;
-    wordwrap($msg);
+    $msg = "Wiadomość od: ".$od."\n".$tresc;
+    
     mail('example@domain.com.pl','Subject',$msg);
     header('location:index.html#kontakt');
 ?>
