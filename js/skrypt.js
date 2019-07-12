fccc44ec719dcb9a7d85d700b82edd5fb516d08b
michałBazan <michalbaz@poczta.onet.pl>
2019-07-12 02:43:45 +0200

    modyfikacja skryptu, fonty do naprawy (nieokreślony błąd :/ )

--- js/skrypt.js
+++ js/skrypt.js
@@ -1,39 +1,55 @@
-//formularz popup
-
 function napisz(){
-   var msg = document.getElementById('msg');
-   msg.style.display = 'block';
-}
-function zamknij(){
     var msg = document.getElementById('msg');
-    msg.style.display = 'none';
-}
-function walidacja(){
-    var formularz = document.forms[0];
-    var mailExp = /^[0-9a-zA-Z-_.]{1,}@[0-9a-zAZ-_.]{1,}\.[a-zAZ]{2,}$/;
-    
-    if(formularz.wiadomosc.value==''||!mailExp.test(formularz.email.value)){
-        if(formularz.wiadomosc.value==''){
-            formularz.wiadomosc.style='border:1px solid red; border-radius:5px;';
-        }
-        else{
-            formularz.wiadomosc.style='border:1px solid green; border-radius:5px;';
-        }
+    msg.style.display = 'block';
+ }
+ 
+ var button = document.getElementById('napisz');
+ button.addEventListener('click',napisz,false);
+ 
+ var button1 = document.getElementById('napisz1');
+ button1.addEventListener('click',napisz,false);
+ 
+ function zamknij(){
+     var msg = document.getElementById('msg');
+     msg.style.display = 'none';
+ }
+ 
+ var button2 = document.getElementById('msg').firstChild;
+ button2.addEventListener('click',zamknij,false);
+ 
+ function waliduj(){
+     var mailExp = /^[0-9a-zA-Z-_.]{1,}@[0-9a-zAZ-_.]{1,}\.[a-zAZ]{2,}$/;
+     var email = document.getElementById('email');
+     var txt = document.getElementById('wiadomosc');
+     if(mailExp.test(email.value)&&txt.value!='')
+         document.forms[0].submit();
+ }
+ var klik = document.getElementById('button');
+ klik.addEventListener('click',waliduj,false);
+ 
+ function isEmailNull(){
+     var mailExp = /^[0-9a-zA-Z-_.]{1,}@[0-9a-zAZ-_.]{1,}\.[a-zAZ]{2,}$/;
+     var email = document.getElementById('email');
+     if(!mailExp.test(email.value)){
+        email.style='border:1px solid red; border-radius:5px;';
+     }
+     else{
+        email.style='border:1px solid green; border-radius:5px;';
+     }
+ }
 
-        if(!mailExp.test(formularz.email.value)){
-            formularz.email.style='border:1px solid red; border-radius:5px;';
-        }
-        else{
-            formularz.email.style='border:1px solid green; border-radius:5px;';
-        }
+ function isTxtNull(){
+    var txt = document.getElementById('wiadomosc');
+    if(txt.value==''){
+       txt.style='border:1px solid red; border-radius:5px;';
     }
     else{
-        formularz.submit();
+       txt.style='border:1px solid green; border-radius:5px;';
     }
 }
-$('.slider').slick({
-    slidesToShow: 5,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-  });
+ 
+ var email = document.getElementById('email');
+ var txt = document.getElementById('wiadomosc');
+ 
+ email.addEventListener('blur',isEmailNull,false);
+ txt.addEventListener('blur',isTxtNull,false);
