4df4daa873ad49a66122936cd0e6ae81d87c2c4b
michałBazan <michalbaz@poczta.onet.pl>
2019-07-12 11:36:56 +0200

    php

---
+++ php/send.php
@@ -0,0 +1,8 @@
+<?php
+    $od = $_POST['email']; 
+    $tresc = $_POST['wiadomosc'];
+    $_POST = array();
+
+    //mail('example@domain.com.pl','Subject',$tresc,$od);
+    //header('location:index.php');
+?>
--- send.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $od = $_POST['email']; 
-    $tresc = $_POST['wiadomosc'];
-    $_POST = array();
-    $msg = "Wiadomość od: ".$od."\n".$tresc;
-    
-    mail('example@domain.com.pl','Subject',$msg);
-    header('location:index.html#kontakt');
-?>
